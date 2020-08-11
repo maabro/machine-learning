8a6966db6ceeb852864693495213a789810a497a
maabro <marcos.abaurrea.rodriguez@gmail.com>
2020-08-11 10:58:20 +0200

    Visualización de los datos, parte final

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,5 +1,6 @@
 # Plots
 import pandas as pd # https://pandas.pydata.org/docs/
+import numpy as np
 import os # https://docs.python.org/3/library/os.html
 import matplotlib.pyplot as plt # https://matplotlib.org/contents.html
 
@@ -9,7 +10,30 @@ full_path = os.path.join(main_path, file_name) # carga el path completo
 
 data = pd.read_csv(full_path)
 
+print(data.head())
+
 # Scatter Plot
-data.plot(kind ="scatter", x="Day Mins", y="Day Charge")
+figure, axs = plt.subplots(2,2, sharey=True, sharex=True)
+data.plot(kind ="scatter", x="Day Mins", y="Day Charge", ax=axs[0][0])
+data.plot(kind ="scatter", x="Night Mins", y="Night Charge", ax=axs[0][1])
+data.plot(kind ="scatter", x="Day Calls", y="Day Charge", ax=axs[1][0])
+data.plot(kind ="scatter", x="Night Calls", y="Night Charge", ax=axs[1][1])
+
 # https://datacarpentry.org/python-ecology-lesson-es/08-putting-it-all-together/index.html
 plt.show() # mostrar el gráfico
+
+# Histograma de frecuencias
+k = int(np.ceil(1+np.log2(3333))) # https://es.wikipedia.org/wiki/Regla_de_Sturges
+plt.hist(data['Day Calls'], bins = k) # https://matplotlib.org/3.3.0/api/_as_gen/matplotlib.pyplot.hist.html
+plt.xlabel("Número de llamadas al día")
+plt.ylabel("Frecuencia")
+plt.title("Histograma del número de llamadas al día")
+
+plt.show()
+
+# Boxplot, diagrama de caja y bigotes
+plt.boxplot(data['Day Calls']) # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.boxplot.html
+plt.ylabel("Número de llamadas diarias")
+plt.title("Boxplot de las llamadas diarias")
+
+plt.show()
